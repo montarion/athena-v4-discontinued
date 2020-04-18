4a08596eb71c5d6782f2d1d1f47fcbf796d57919
jamiro <jamirograntsaan@gmail.com>
2020-04-19 00:45:49 +0100

    prepared for website implementation

--- start.py
+++ start.py
@@ -1,14 +1,16 @@
 from components.networking import Networking
 from components.modules import Modules
+from components.website import website
 
 import threading
 from time import sleep
 
 t1 = threading.Thread(target=Networking().startserving)
 t2 = threading.Thread(target=Modules().standard)
+t3 = threading.Thread(target=website().runsite)
 
 
 t1.start()
 t2.start()
-
+#t3.start()
 
