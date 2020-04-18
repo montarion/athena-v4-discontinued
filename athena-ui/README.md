33050fb35a9b5995c510ad4368ee9f3768d0d844
Christian van Dijk <christian.vandijk@student.hu.nl>
2020-04-18 15:24:19 +0200

    SPA frontend made with lit-element

--- athena-ui/README.md
+++ athena-ui/README.md
@@ -1,24 +1,6 @@
-# athena-ui
+# Athena V4 UI
 
-## Project setup
+## Install & Run
+```bash
+    npx serve
 ```
-npm install
-```
-
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ athena-ui/pages/Anime.js
@@ -0,0 +1,56 @@
+import {
+    LitElement,
+    html,
+    css
+} from 'https://unpkg.com/lit-element@2.1.0/lit-element.js?module'
+
+class AnimePage extends LitElement {
+    render() {
+        return html`
+        <div class= "anime">
+            <div class="main">
+                <h1 class="title">Anime</h1>
+                <div class="content">
+                    <div class="card"></div>
+                    <div class="card"></div>
+
+                    <div class="card"></div>
+                    <div class="card"></div>
+
+                    <div class="card"></div>
+                    <div class="card"></div>
+                </div>
+            </div>
+        </div>
+    `;
+    }
+    static get styles() {
+        return css`
+        .card {
+            background-image: url(https://tokyo.nl/wp-content/uploads/2014/10/manga-tekeningen.jpg);
+            background-color: white;
+            flex-basis: 40%;
+            min-height: 15em;
+            border-radius: 2em;
+            margin-top: 2em;
+          
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+          }
+          
+          .content {
+            display: flex;
+            flex-wrap: wrap;
+            flex-grow: 1;
+            justify-content: space-evenly;
+          }
+          .title {
+            color: white;
+          }
+        `;
+    }
+}
+
+customElements.define('anime-page', AnimePage)
