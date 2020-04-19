3f6f93b1231a3437d6a2bc0661ac64d209a5e4be
Christian van Dijk <christian.vandijk@student.hu.nl>
2020-04-19 19:50:38 +0200

    websocket tr-out

--- athena-ui/main.js
+++ athena-ui/main.js
@@ -10,11 +10,26 @@ console.log('%c Athena: Hello', 'background-color: black; color: blue;')
 class AthenaRouterOutlet extends LitElement {
   static get properties() {
     return {
-      route: { type: Object }
+      route: { type: Object },
+      client: { type: Object },
+      id: { type: Object } // id can be between 0-999  
     }
   }
   constructor() {
     super()
+    this.id = Math.floor(Math.random() * Math.floor(999));
+    console.log(`Connecting to socket as: ${this.id}`)
+
+    this.client = new WebSocket("wss://echo.websocket.org"); // this socket echo's everything back that you send to it
+    this.client.onopen = () => {
+      console.log('connected to socket:', this.client.url)
+      this.client.send(`Hello from ${this.id}`)
+    };
+
+    this.client.onmessage = (event) => {
+      console.log('RECEIVED: ' + event.data);
+    };
+
     let router = new Navigo('/', true, '#!')
     router
       .on('home', () => {
@@ -41,6 +56,12 @@ class AthenaRouterOutlet extends LitElement {
       })
     router.resolve()
   }
+
+  disconnectedCallBack() { // on element Destroy
+    this.client.destroy(); // kill client
+    super.disconnectedCallBack()
+  }
+
   render() {
     return html`
       <div>
