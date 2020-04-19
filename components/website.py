af87f4c8e9ff72f1dbdef32ebfff65fe14b8a8ca
jamiro <jamirograntsaan@gmail.com>
2020-04-19 19:11:48 +0100

    website can now be run standalone

--- components/website.py
+++ components/website.py
@@ -1,5 +1,9 @@
-from Flask import flask, render_template
-from flask_sockets import sockets
+from flask import Flask, render_template
+from flask_sockets import Sockets
+
+from gevent import pywsgi
+from geventwebsocket.handler import WebSocketHandler
+
 import os, json, redis
 
 
@@ -7,21 +11,29 @@ class website:
     def __init__(self):
         self.app = Flask(__name__)
         self.sockets = Sockets(self.app)
-
     def runserver(self):
 
-        @sockets.route('/echo')
-        def echo_socket(ws):
-            while not ws.closed:
-                message = ws.receive()
-                ws.send(message)
+        @self.sockets.route('/')
+        def socket(ws):
+            try:
+                while not ws.closed:
+                    message = ws.receive()
+                    print(f"Message received: {message}")
+                    ws.send(message)
+            except webSocketError:
+                print("lost connection!")
+            except Exception as e:
+                print("ERROR!")
+                print(e.__class__.__name__)
 
-        @sockets.route("/anime")
-        def anime(ws):
-            while not ws.closed:
-                message = ws.receive()
-
-        @app.route('/')
+        @self.app.route('/')
         def hello():
             return render_template("index.html")
 
+
+        server = pywsgi.WSGIServer(('0.0.0.0', 8080), self.app, handler_class=WebSocketHandler)
+        server.serve_forever()
+
+if __name__ == "__main__":
+    print("Starting as standalone")
+    website().runserver()
