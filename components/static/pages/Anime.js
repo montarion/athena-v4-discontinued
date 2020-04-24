a9b851e57cb865ced25c58f52153ed1fc35b8dd8
jamiro <jamirograntsaan@gmail.com>
2020-04-24 16:51:46 +0100

    added sorting by epoch

--- components/static/pages/Anime.js
+++ components/static/pages/Anime.js
@@ -25,7 +25,7 @@ class AnimePage extends LitElement {
         this.setPageHandler();
         this.getAnimeList();
         var dtf = new Intl.DateTimeFormat('en', { year: 'numeric', month: 'short', day: '2-digit' });
-        
+
     }
 
     disconnectedCallBack() { // on element Destroy
@@ -46,8 +46,10 @@ class AnimePage extends LitElement {
             networking.sendmessage({ category: "anime", type: "list" },
                 (animeList) => { // pass the callback function
                     self.animeList = animeList.data.list; //set latestAnime in Home.js
+                    self.animeListHorizontal = self.animeList.slice(); // clone without reference
+                    self.animeList.sort(function(a,b){return b.aired_at - a.aired_at}); // sort by epoch
+                    self.animeListHorizontal.sort(function(a,b){return a.aired_at - b.aired_at}); // same, but reverse, since that way it'll end up left-to-right
                     console.log(self.animeList);
-                    console.log(self.animeList[0].aired_at);
                 });
 
         }).catch(error => { // errors with socket connection end up here
@@ -73,22 +75,44 @@ class AnimePage extends LitElement {
         return html`
         <div class= "anime">
             <div class="main">
-                <h1 class="title">Anime</h1>
+                <!-- <h1 class="title">Anime</h1> -->
                 <div class="content">
+                <div class="card" style="margin-top: 0; padding-top: 0; margin-bottom: 3em;">
+                Selected-Anime
+                </div>
+                <div class="horizontal-list"> 
+                    ${this.animeListHorizontal.map(anime => {
+            return html`
+                        <div class="small-card" style="background-image: linear-gradient(to top, rgba(0,0,0, 0.8), rgba(0,0,0, 0.0)), url('${anime.art.cover}'); background-size: cover">
+                            <div class="small-card-content">
+                                    ${anime.title}
+                            </div>
+                        </div>
+                        `;
+        })}
+                    </div>
+
+                    <!-- Display "selectedAnime"-single-card instead of this cards-list -->
                     ${this.animeList.map(anime => {
             return html`
                         <div class="card" style="background-image: linear-gradient(to top, rgba(0,0,0, 0.8), rgba(0,0,0, 0.0)),
                         url('${anime.art.cover}'); background-size: cover">
-                        ${anime.title}<br> Latest episode: ${anime.lastep} <br>
-                        Aired on: ${new Date(anime.aired_at *1000).toUTCString()} // more shows will be getting it
+                            <div style="display: flex; flex-direction: column; justify-content: center;">
+                                <p>
+                                    ${anime.title}
+                                </p>
+                                <p>
+                                    Latest episode: ${anime.lastep}
+                                </p>
+                                <p>
+                                    Aired on: ${new Date(anime.aired_at * 1000).toUTCString()}
+                                </p> <!-- more shows will be getting it -->
+                            </div>
                         </div>
                         `;
         })}
-                    <!-- <div class="card"></div>
-                    <div class="card"></div>
 
-                    <div class="card"></div>
-                    <div class="card"></div> -->
+
                 </div>
             </div>
         </div>
@@ -96,9 +120,36 @@ class AnimePage extends LitElement {
     }
     static get styles() {
         return css`
+        .horizontal-list {
+            overflow: auto;
+            white-space: nowrap;
+            margin-left: 3em;
+        }
+
+        .small-card {
+            display: inline-block;
+            background-color: white;
+            border-radius: 2em;
+            
+            min-width: 20em;
+            max-width: 20em;
+            min-height: 15em;
+            margin-right: 1em;
+            margin-bottom: 1em;
+            // word-break: break-all;
+
+            color: white;
+            font-size: 12px;
+
+            box-shadow: 0 1rem 2rem 0 rgba(0,0,0,0.3);
+        }
+
+        .small-card-content {
+            padding-top: 2em;
+        }
+
         .card {
-            background-image: linear-gradient(to top, rgba(0,0,0, 0.8), rgba(0,0,0, 0.0)), 
-                url(https://tokyo.nl/wp-content/uploads/2014/10/manga-tekeningen.jpg);
+            background-image: linear-gradient(to top, rgba(0,0,0, 0.8), rgba(0,0,0, 0.0));
             background-color: white;
             flex-basis: 40%;
             min-height: 15em;
@@ -112,6 +163,12 @@ class AnimePage extends LitElement {
 
             color: white;
             font-size: 26px;
+
+            box-shadow: 0 1rem 2rem 0 rgba(0,0,0,0.3);
+            transition: box-shadow 0.5s ease-in-out;
+          }
+          .card:hover {
+            box-shadow: 0 1rem 2rem 0 rgba(0,0,0,0.8);
           }
           
           .content {
